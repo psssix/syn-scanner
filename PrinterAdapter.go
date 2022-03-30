d3a06990c64b304bc0e9b32854b6605c0914bceb
psssix <psssix@gmail.com>
2022-03-30 23:36:17 +0300

    feat: add printer adapter for di on print function

---
+++ PrinterAdapter.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+type PrinterAdapter struct{}
+
+func (p PrinterAdapter) Print(args ...interface{}) {
+	fmt.Print(args...)
+}
+
+func (p PrinterAdapter) Printf(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
