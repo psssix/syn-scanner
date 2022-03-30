766fdc9d377cf87c9b79c514f51a910ec29b556b
psssix <psssix@gmail.com>
2022-03-30 23:37:22 +0300

    feat: assemble scanner

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"os"
+	"time"
+)
+
+const defaultThreadCount = 8
+const dialerTimeout = 10
+
+func main() {
+	target := flag.String("t", "", "target for scanning")
+	threads := flag.Int("s", defaultThreadCount, "number of threads(streams) when scanning")
+	flag.Parse()
+	if *target == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	newScanner(
+		newProducer(),
+		newWorker(&net.Dialer{Timeout: dialerTimeout * time.Second}),
+		newReporter(PrinterAdapter{}),
+	)(*target, *threads)
+}
