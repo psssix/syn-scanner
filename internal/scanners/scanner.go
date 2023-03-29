f37c9928054b5b74af4c804dcdece18148db3e2b
П$iх <psssix@gmail.com>
2023-03-29 23:37:21 +0300

    refactor: scanner types refactoring

--- internal/scanners/scanner.go
+++ internal/scanners/scanner.go
@@ -6,17 +6,25 @@ import (
 	"github.com/psssix/syn-scanner/pkg/producers"
 )
 
-func NewScanner(
-	producer func(from, to int, ports chan<- int),
-	worker func(target string, ports <-chan int, opened chan<- int),
-	reporter func(target string, opened <-chan int),
-) func(address string, threads int) {
+type (
+	Producer func(from, to int, ports chan<- int)
+	Worker   func(target string, ports <-chan int, opened chan<- int)
+	Reporter func(target string, opened <-chan int)
+	Scanner  func(target string, threads int)
+)
+
+func NewScanner(producer Producer, worker Worker, reporter Reporter) Scanner {
 	return func(target string, threads int) {
-		waitScanner := sync.WaitGroup{}
-		ports := make(chan int, threads)
-		opened := make(chan int, threads)
+		var (
+			waitScanner = sync.WaitGroup{}
+			waitWorkers = sync.WaitGroup{}
+
+			ports  = make(chan int, threads)
+			opened = make(chan int, threads)
+		)
 
 		waitScanner.Add(1)
+
 		go func() {
 			defer func() {
 				close(ports)
@@ -26,27 +34,32 @@ func NewScanner(
 		}()
 
 		waitScanner.Add(1)
+
 		go func() {
 			defer func() {
 				close(opened)
 				waitScanner.Done()
 			}()
-			waitWorkers := sync.WaitGroup{}
+
 			for i := 0; i < threads; i++ {
 				waitWorkers.Add(1)
+
 				go func() {
 					defer waitWorkers.Done()
 					worker(target, ports, opened)
 				}()
 			}
+
 			waitWorkers.Wait()
 		}()
 
 		waitScanner.Add(1)
+
 		go func() {
 			defer waitScanner.Done()
 			reporter(target, opened)
 		}()
+
 		waitScanner.Wait()
 	}
 }
